04a27520193594f0cee5275e1dc41b8d8c8aa1ba
Ekaterina Belyakova <Ekaterina_Belyakova@epam.com>
2020-11-23 17:20:29 +0300

    feat: implement adding movie

--- src/movies-api.ts
+++ src/movies-api.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import fs from 'fs';
 import movies from './data/data.json';
 
 export const router = Router();
@@ -13,7 +13,8 @@ router.get('/search', (req, res) => {
     const { request, type } = req.query;
 
     res.send(movies.Search.filter(
-        (movie) => movie.Title.toLowerCase().includes((request as string).toLowerCase()) &&
+        (movie) => movie.Title.toLowerCase().search((request as string)?.toLowerCase()) !== -1 && // npm query-string
+        // (movie) => movie.Title.toLowerCase().includes((request as string).toLowerCase()) &&
                    movie.Type === type));
 });
 
@@ -21,4 +22,29 @@ router.get('/movie/:movieId', (req, res) => {
     const { movieId } = req.params;
 
     res.send(movies.Search.find((movie) => movie.imdbID === movieId));
+});
+
+router.post('/add-movie', (req, res) => {
+    if (movies.Search.find((movie) => movie.imdbID === req.body.imdbID)) {
+        res.status(200).send({
+            text: `Movie with imdbID ${req.body.imdbID} already exists!`,
+            status: 'error'
+        });
+    } else {
+        movies.Search.push(req.body);
+        const json = {
+            Search: movies.Search
+        };
+
+        fs.writeFile('src/data/data.json', JSON.stringify(json, null, 2), (error) => {
+            if (error) {
+                throw error;
+            }
+        });
+
+        res.status(200).send({
+            text: `Movie '${req.body.Title}' added!`,
+            status: 'success'
+        });
+    }
 });
